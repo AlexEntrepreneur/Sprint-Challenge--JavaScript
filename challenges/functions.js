981d82be9f92500f5ca367fa3311e95ae99f9865
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-01-25 10:48:21 +0000

    Complete "Callbacks" Challenges

--- challenges/functions.js
+++ challenges/functions.js
@@ -3,9 +3,13 @@
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
   * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
+
+function consume(a, b, callback) {
+  return callback(a, b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,6 +17,28 @@
   * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+function add(a, b, callback) {
+  if (typeof a === 'number' && typeof b === 'number') {
+  return a + b;
+  }
+  else {
+    return `${add.name}: Please enter a number`;
+  }
+}
+
+function multiply(a, b) {
+  if (typeof a === 'number' && typeof b === 'number') {
+  return a * b;
+  }
+  else {
+    return `${multiply.name}: Please enter a number`;
+  }
+}
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
